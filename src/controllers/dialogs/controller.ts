ab0cca9acb90f928d22c288cf8468d56e0bda308
Danila Platonov <danilaplatonov209@gmail.com>
2025-10-28 13:37:29 +0300

    fix getting user dialogs

--- src/controllers/dialogs/controller.ts
+++ src/controllers/dialogs/controller.ts
@@ -7,13 +7,50 @@ class DialogsController {
     static async getUserDialogs(req: Request, res: Response) {
         try {
             const user = await userHelpers.getUserFromToken(req);
-            const dialogs = await Dialogs.find({ members: user?._id })
-                .populate({
-                    path: 'members',
-                    select: 'name avatar', 
-                    model: User
-                })
-                .exec();
+            const dialogs = await Dialogs.aggregate([
+                {
+                    $match: {
+                        "members": user?._id.toString()
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        let: { memberIds: "$members" },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $in: [
+                                            { $toString: "$_id" },
+                                            "$$memberIds"
+                                        ] 
+                                    }
+                                }
+                            },
+                            {
+                                $project: {
+                                    _id: 1,
+                                    name: 1,
+                                    avatar: 1
+                                }
+                            }
+                        ],
+                        as: "members_data"
+                    }
+                },
+                {
+                    $addFields: {
+                        members: "$members_data"
+                    }
+                },
+                {
+                    $project: {
+                        members_data: 0
+                    }
+                }
+            ]);
+
             res.status(200).json({ message: "Информация о диалогах успешно получена", dialogs });
         }
         catch (error) {
